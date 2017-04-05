1f21c58e478cf02e9a997c8bd9a00ebc45e1ebdc
Sergiy Grytsenko <gritcsenko@gmail.com>
2017-04-05 15:11:33 +0300

    Wifi connection is more transparent

--- lib/Wifi/Wifi.h
+++ lib/Wifi/Wifi.h
@@ -3,6 +3,8 @@
 
 bool InitWifiSta(JsonObject& settingsRoot)
 {
+  WiFi.mode(WIFI_OFF);
+
   JsonObject& wifi_sta = settingsRoot["sta"].as<JsonObject&>();
 
   Serial.print("Connecting to WIFI ");
@@ -12,11 +14,40 @@ bool InitWifiSta(JsonObject& settingsRoot)
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_sta["ssid"].as<char*>(), wifi_sta["password"].as<char*>());
 
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  int tryCounter = 20;
+  while(!WiFi.isConnected() && tryCounter-- > 0)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println(".");
+
+  if(!WiFi.isConnected())
+  {
     Serial.println("Connection Failed!");
     WiFi.mode(WIFI_OFF);
     return false;
   }
 
+  Serial.print("localIP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("macAddress: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("subnetMask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("gatewayIP: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("hostname: ");
+  Serial.println(WiFi.hostname());
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("psk: ");
+  Serial.println(WiFi.psk());
+  Serial.print("BSSID: ");
+  Serial.println(WiFi.BSSIDstr());
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  WiFi.setAutoConnect(true);
+  WiFi.setAutoReconnect(true);
   return true;
 }
